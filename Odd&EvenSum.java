5b49589dcdbdeffc4db872fa3a8afe1947fd0669
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-02-15 21:47:31 +0530

    Update Odd&EvenSum.java

--- Odd&EvenSum.java
+++ Odd&EvenSum.java
@@ -1,1 +1,18 @@
 // Array containing Even and Odd numbers and printing their sum
+
+class Main {
+
+public static void main(String[] args) {
+
+int x[]={1,2,3,5,6,7,8);
+
+int odd-0;
+
+int even-0;
+
+for (int i=0; 17; 1++) {
+
+if(x[1]%2==0){
+
+even-even+x[i];
+
