a52f4ba5dc150629898835ed9e2dbe137a8ac7d9
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-02-20 07:20:17 +0530

    Update J_07_Ques.java

--- J_07_Ques.java
+++ J_07_Ques.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+
+public class J_07_Ques {
+    public static void main(String[] args) {
+        
+        // Qs 1
+
+        // int a = 10;
+        // int b =20;
+        // int c = 30;
+        // int sum = a + b + c;
+        // System.out.println(sum);
+
+        // Qs 2
 
