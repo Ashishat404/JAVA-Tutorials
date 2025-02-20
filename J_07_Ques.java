2621ba3c5f7b06369311812605da864af0447a43
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-02-20 07:21:28 +0530

    Update J_07_Ques.java

--- J_07_Ques.java
+++ J_07_Ques.java
@@ -13,3 +13,25 @@ public class J_07_Ques {
 
         // Qs 2
 
+        // float Sub1 = 44;
+        // float Sub2 = 55;
+        // float Sub3 = 66;
+        // float cgpa = (Sub1 + Sub2 + Sub3 )/30;
+        // System.out.println(cgpa);
+
+        // Qs 3  --> Addition of string
+
+        // System.out.println("What is your name");
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.next();
+        // System.out.println("Hello " + name + " Have a Nice Day! ");
+
+        // Qs 5 
+        System.out.println("ENter your Number");
+         Scanner sc = new Scanner(System.in);
+         System.out.println(sc.hasNextInt());
+    
+    }
+}
+
+
