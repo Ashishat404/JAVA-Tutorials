af07e4b6002009a20dfb33fb872f1f2833d55671
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-02-20 07:19:06 +0530

    Update J_04_Literals.java

--- J_04_Literals.java
+++ J_04_Literals.java
@@ -1,1 +1,26 @@
 
+public class J_04_Literals{ 
+    public static void main(String[] args) {
+        byte age = 34;
+        int ABC = 11;
+        short dog = 99;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d = 3.11;
+        long Age = 77777777L;
+        boolean a = true;
+
+
+        System.out.println(ABC);
+        System.out.println(dog);
+        System.out.println(f1);
+        System.out.println(age);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(Age);
+        System.out.println(a);
+
+        String str = "Ashish is a string literal";
+        System.out.println(str);
+    }
+}
