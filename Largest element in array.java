b77b981f86cf3ad7b4ea85eaeda2a54ef080ecbb
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-03-08 12:03:28 +0530

    Update Largest element in array.java

--- Largest element in array.java
+++ Largest element in array.java
@@ -1,4 +1,7 @@
 public class LargestElement {
 public static void main(String[] args) {
         int[] array = {10, 20, 30, 5, 50, 40, 15};
-        
+      int max = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+              
