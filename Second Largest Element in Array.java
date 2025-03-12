8229a1c901589832be9f00a736786cca0275ddd6
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-03-12 22:50:03 +0530

    Update Second Largest Element in Array.java

--- Second Largest Element in Array.java
+++ Second Largest Element in Array.java
@@ -1,1 +1,6 @@
 //This is program in java for finding second largest element in array 
+public class SecondLargestElement {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 5, 50, 40, 15};
+
+        
