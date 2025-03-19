411cdbf6c32e20b5ed4a959fffdfc5059a9a85fb
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-03-19 22:49:14 +0530

    Update  fibonacci series.java

--- Othr/ fibonacci series.java
+++ Othr/ fibonacci series.java
@@ -1,4 +1,6 @@
 public class FibonacciSeries {
     public static void main(String[] args) {
         int n = 10; // Number of terms in the Fibonacci series
+        int firstTerm = 0, secondTerm = 1;
+
         
