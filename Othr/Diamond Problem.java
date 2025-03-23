1416a5a0ac6bcdaeb84d0eb21832274dc6bba78b
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-03-24 00:38:34 +0530

    Update Diamond Problem.java

--- Othr/Diamond Problem.java
+++ Othr/Diamond Problem.java
@@ -18,8 +18,7 @@ interface C extends A {
 }
 
 class D implements B, C {
-    // Resolve the diamond problem by 
-overriding the display method
+    // Resolve the diamond problem by overriding the display method
     @Override
     public void display() {
         // Option 1: Call a specific 
