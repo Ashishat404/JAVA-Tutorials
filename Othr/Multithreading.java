a093c02e0b8cc0a2cf453bb239d82c308907ecbc
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-04-09 15:27:49 +0530

    Update Multithreading.java

--- Othr/Multithreading.java
+++ Othr/Multithreading.java
@@ -1,4 +1,8 @@
 class MyThread extends Thread {
     public void run() {
         for (int i = 0; i < 5; i++) {
-            
+System.out.println(Thread.currentThread().getId() + " Value: " + i);
+        }
+    }
+}
+
