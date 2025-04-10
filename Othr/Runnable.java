55f4a6b63e5e95de33a15e5f1b1ab554b88c59c1
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-04-10 19:11:26 +0530

    Update Runnable.java

--- Othr/Runnable.java
+++ Othr/Runnable.java
@@ -1,4 +1,8 @@
 class MyRunnable implements Runnable {
     public void run() {
         for (int i = 0; i < 5; i++) {
-            
+            System.out.println(Thread.currentThread().getId() + " Value: " + i);
+        }
+    }
+}
+
