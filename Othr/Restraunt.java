1fd4bc33b08b93676902bba1fc9065b4af8b6b19
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-04-20 20:19:33 +0530

    Update Restraunt.java

--- Othr/Restraunt.java
+++ Othr/Restraunt.java
@@ -1,7 +1,8 @@
 class Restaurant {
     private boolean foodReady = false;
-
-  // Serve food method (Customer waits until food is ready)
-    public synchronized void serveFood(){
-while (!foodReady) {
+ // Serve food method (Customer waits until food is ready)
+    public synchronized void serveFood() {
+        while (!foodReady) {
             try {
+
+                
