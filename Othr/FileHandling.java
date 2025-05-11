fc33ea5d54ad7cf299967f06a1bb3c5345f51703
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-05-11 21:25:24 +0530

    Update FileHandling.java

--- Othr/FileHandling.java
+++ Othr/FileHandling.java
@@ -4,4 +4,7 @@ import java.io.IOException;
 public class FileHandling {
     public static void main(String[] args) {
         try {
+            // Specify the file name and path
+            File file = new File("example.txt");
+
             
