2edb97842e21f820fee39240d8d615d98b87137e
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-05-11 21:26:16 +0530

    Update FileHandling.java

--- Othr/FileHandling.java
+++ Othr/FileHandling.java
@@ -5,6 +5,9 @@ public class FileHandling {
     public static void main(String[] args) {
         try {
             // Specify the file name and path
-            File file = new File("example.txt");
-
-            
+            File file = newFile("example.txt");
+            // Create the file
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                  
