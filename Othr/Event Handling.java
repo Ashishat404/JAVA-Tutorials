cdff4484b8124313285dd1461f4a99b76d46053d
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-05-25 16:10:35 +0530

    Update Event Handling.java

--- Othr/Event Handling.java
+++ Othr/Event Handling.java
@@ -6,3 +6,6 @@ public class EventExample {
         Frame f = new Frame("Event Handling Example");
         Button b = new Button("Click Me");
         b.setBounds(50, 100, 60, 30);
+
+        // Registering an event listener
+        
