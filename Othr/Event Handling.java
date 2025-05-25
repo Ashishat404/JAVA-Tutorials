ce7bfda911e9332d96571aab8848a2655ab4ee0a
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-05-25 16:09:55 +0530

    Update Event Handling.java

--- Othr/Event Handling.java
+++ Othr/Event Handling.java
@@ -4,4 +4,5 @@ import java.awt.event.*;
 public class EventExample {
     public static void main(String[] args) {
         Frame f = new Frame("Event Handling Example");
+        Button b = new Button("Click Me");
         
