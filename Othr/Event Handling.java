69be7211416566e4646b74fa28cba25462127f4b
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-05-25 16:10:19 +0530

    Update Event Handling.java

--- Othr/Event Handling.java
+++ Othr/Event Handling.java
@@ -5,4 +5,4 @@ public class EventExample {
     public static void main(String[] args) {
         Frame f = new Frame("Event Handling Example");
         Button b = new Button("Click Me");
-        
+        b.setBounds(50, 100, 60, 30);
