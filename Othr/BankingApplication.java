387b5dfada0aa0b8966d4c40987b7ad579aa7418
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-05-27 20:14:18 +0530

    Create BankingApplication.java

---
+++ Othr/BankingApplication.java
@@ -0,0 +1,5 @@
+//Develop a program for banking application with exception handling. Handle the exceptions in following cases: 
+//-> Account balance <1000 
+//-> Withdrawal amount is greater than balance amount 
+//-> Transaction count exceeds 3 
+//-> One day transaction exceeds 1 lakh. 
