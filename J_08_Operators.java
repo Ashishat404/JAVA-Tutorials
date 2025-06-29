44a6ee5dd3776294c4de50f7bac39f5fc58e9d28
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-06-29 09:17:57 +0530

    Update J_08_Operators.java

--- J_08_Operators.java
+++ J_08_Operators.java
@@ -2,4 +2,6 @@ public class J_08_Operators {
     public static void main(String[] args) {
     // 1. Arithmatic Operators 
         int a = 4;
-      //int b = 6 % a; 
+      //int b = 6 % a;       // Modulo Operator
+    //   4.8%1.1 --> Returns decimal remainder
+        // Arithmatic operators cannot work with booleans
