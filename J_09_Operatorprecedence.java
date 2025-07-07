c78acc231718b45e658d26f673ada6efdc033ba7
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-07-07 08:43:12 +0530

    Update J_09_Operatorprecedence.java

--- J_09_Operatorprecedence.java
+++ J_09_Operatorprecedence.java
@@ -4,3 +4,8 @@ public class J_09_OperatorPrecedence {
     // No BODMAS works here 
         int a = 6*5-34/2;
         /*
+            highest precedence goes to * and / they are then evalueated on the basis of left to right associativity.
+         = 30-34/2
+         = 30-17
+         =13
+                */
