4a10be52dae042f8e3fa44597eb00d99882408b8
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-07-08 16:29:09 +0530

    Update J_09_Quiz.java

--- J_09_Quiz.java
+++ J_09_Quiz.java
@@ -4,5 +4,7 @@ public class J_09_Quiz {
         int x = 6;
         int y = 1;
         // int k = x*y/2;
-         int b = 1;
+        int b = 1;
         int c = 4;
+        int a = 5; 
+        int k = b*b - (4*a*c)/(2*a);
