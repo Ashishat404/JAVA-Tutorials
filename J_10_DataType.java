a5d86eab1a3de10db7ea1c83e2186bea57043bd5
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-07-10 11:46:17 +0530

    Update J_10_DataType.java

--- J_10_DataType.java
+++ J_10_DataType.java
@@ -1,8 +1,11 @@
 public class J_10_DataType {
     public static void main(String[] args) {
-       byte x = 5;
-       int y = 6;
-       short z = 8;
-       int a = y + z;
-       float b = 6.54f + x;
-       System.out.println(b);
+        byte x = 5;
+        int y = 6;
+        short z = 8;
+        int a = y + z;
+        float b = 6.54f + x;
+        System.out.println(b);
+
+        // Increment and Decrement Operator
+         int i = 56;
