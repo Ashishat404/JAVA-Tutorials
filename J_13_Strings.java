f46e5dadc0d6539b132628326fe3323fb548fc0e
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-07-13 10:21:21 +0530

    Update J_13_Strings.java

--- J_13_Strings.java
+++ J_13_Strings.java
@@ -7,3 +7,5 @@ public class J_13_Strings {
        // System.out.print("the name is : "); // println prints string & gives moves cursor to a new line.
        // whereas the prints judt prints  everything in just one single line it does not move cursor to a new line.
        // System.out.print(name);
+
+        /* int a = 6;
