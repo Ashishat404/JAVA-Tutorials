ac94de1fef0ae54a4013248c56a5b69aeb9582e9
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-07-13 10:19:47 +0530

    Update J_13_Strings.java

--- J_13_Strings.java
+++ J_13_Strings.java
@@ -4,3 +4,5 @@ public class J_13_Strings {
     public static void main(String[] args) {
         // String name = new String ("ashish");
         // String name = "ashish";
+       //System.out.print("the name is : "); // println prints string & gives moves cursor to a new line.
+
