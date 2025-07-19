1fc28ad13eb14e20569221f6f3fe48d6fd226f00
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-07-20 00:03:31 +0530

    Update J_13_Strings.java

--- J_13_Strings.java
+++ J_13_Strings.java
@@ -15,4 +15,5 @@ public class J_13_Strings {
         Scanner sc = new Scanner(System.in);
         // String st = sc.next(); // this just takes one word 
         String st = sc.nextLine();
+        System.out.println(st);
         
