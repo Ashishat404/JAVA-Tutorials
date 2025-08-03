585f357158b6c2227f67c043962b949119e61072
Ashishat404 <138343378+Ashishat404@users.noreply.github.com>
2025-08-03 08:35:45 +0530

    Update J_14_StringMeathods.java

--- J_14_StringMeathods.java
+++ J_14_StringMeathods.java
@@ -3,3 +3,4 @@ public class J_14_StringMeathods {
     public static void main(String[] args) {
         String name = "AShish"; // variable's name is name
         //System.out.println(name);
+        int value = name.length();
